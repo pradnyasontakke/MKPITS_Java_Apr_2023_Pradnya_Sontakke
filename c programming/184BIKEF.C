f0171846df1a4fb0c8897fdc477b2a7a173006b9
pradnya sontakke <pradnyasontakke14@gmail.com>
2023-04-21 17:46:46 +0530

    184BIKEF.C

---
+++ c programming/184BIKEF.C
@@ -0,0 +1,12 @@
+#include<conio.h>
+#include<conio.h>
+void main()
+{
+ int num1;float num2;
+ printf("total distance in km");
+ scanf("%d", &num1);
+ printf("spend fuel in liter");
+ scanf("%f",&num2);
+ printf("average (km/liter) \n");
+ getch();
+ }
