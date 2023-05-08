5e5638e8b9d7d5f7bc0e70ac0cc0561feb65f31a
pradnya sontakke <pradnyasontakke14@gmail.com>
2023-05-09 00:44:21 +0530

    9

---
+++ c programming/A.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int num1,num2,res;
+printf("enter the number\n");
+scanf("%d%d",&num1,&num2);
+result=num1+num2;
+printf("result=%d",res);
+getch();
+
+}
