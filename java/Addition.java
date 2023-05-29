e2719ac0b4abe54e695f3ba83ec6da8a8c89d176
Nitesh Dongre <nitesh.dongre22@gmail.com>
2023-05-29 17:51:36 +0530

    29/5/23

---
+++ java/Addition.java
@@ -0,0 +1,39 @@
+ public class Addition
+{
+	/**
+	*@param  l0ine declare scope, return type method name variable declare
+	*return value
+	*18 line object create and call the object
+	*  20print ans.
+	*
+	**/
+	public int addInteger( int number1, int number2)
+	
+			
+	
+	{
+		return number1+number2;
+	}
+	public static void main( String args[])
+	{
+		
+	Addition addition= new Addition();
+		int answer =addition.addInteger(10,30);
+			System.out.println(answer);
+
+		
+		
+		
+
+
+
+	}
+}
+
+
+
+
+
+
+
+
---
+++ java/Addition1.java
@@ -0,0 +1,25 @@
+ public class Addition1{
+
+
+ public static void main(String [] args)
+ {
+ 
+  Addition1 addition_obj = new Addition1();
+  
+  int result=addition_obj.additionOfTwoNumber(100,20);
+  
+  System.out.println(result);
+ 
+
+ {
+  public int additionOfTwoNumber(int number1,int number2)
+    int sum = number1+number2;
+   return sum;
+ }
+ 
+ //public int substractionOfTwoNumber(int number1,int number2){
+ 
+   // int substraction = number1-number2;
+   //return substraction;
+ }
+}
