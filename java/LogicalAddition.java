e26ad662286838adae80622dd30d0abc072d4665
Nitesh Dongre <nitesh.dongre22@gmail.com>
2023-06-10 15:51:39 +0530

    10/6/23

---
+++ java/LogicalAddition.java
@@ -0,0 +1,15 @@
+public class LogicalAddition
+{
+ public int logicalAdd(int number1, int number2, int number3)
+{
+return number1&& number2&&number3;
+}
+public static void main(String args[])
+{
+LogicalAddition logadd = new LogicalAddition();
+int ans =logadd.logicalAdd(12,34,67);
+System.out.println(number1<number2&&number1<number3);
+
+
+}
+}
