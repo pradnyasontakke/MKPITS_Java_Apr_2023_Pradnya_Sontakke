555147e80607132589d93b9c814a5532c6122dc4
Nitesh Dongre <nitesh.dongre22@gmail.com>
2023-09-01 17:10:37 +0530

    ArraysList

--- java/ArraysList/src/ArraysListExample.java
+++ java/ArraysList/src/ArraysListExample.java
@@ -16,6 +16,22 @@ public class ArraysListExample {
             System.out.println(iterator.next());
         }
 
+      arrayList.clone();
+        System.out.println(arrayList);
+
+        ArrayList arrayList1=new ArrayList();
+        arrayList1.add(88);
+        arrayList1.add(54);
+        arrayList1.add(66);
+        System.out.println(arrayList1);
+
+        System.out.println("allAll elements");
+        arrayList.addAll(1,arrayList1);
+        System.out.println(arrayList);
+
+
+        System.out.println(arrayList1.isEmpty());   //return true if empty
+        System.out.println(arrayList1.lastIndexOf(5));
 
 
     }
