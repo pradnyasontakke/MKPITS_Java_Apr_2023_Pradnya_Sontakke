bd1807198475d58e08e99c7e6c9717a3f320f840
Nitesh Dongre <nitesh.dongre22@gmail.com>
2023-09-25 15:39:49 +0530

    sql assignment

--- SQL/studentDatabase.sql
+++ SQL/studentDatabase.sql
@@ -5,16 +5,43 @@ create table students
 (rollno char(5) primary key, studname char(10),
  address varchar(15))
 select * from students
+
+
 create table markdetails
 (rollno char(5), physics int,chemishry int, math int,
 constraint fk_rn foreign key(rollno) references students(rollno))
+
 show table  student
+insert into students values('1001', 'pranish', 'nagpur'),
+						  ('1002','pratik','indore'),
+  insert into students values   ('1003','pratik','pune'),
+                           ('1004','shamita','chandrapurr'),
+							('1005','sami','nagpur');
 select * from students
-insert into students value('1001', 'pranish', 'nagpur')
-insert into students value('1002','pratik','nagpur')
 
-
-insert into markdetails value( '1001',65,87,97)
+insert into markdetails values( '1001',65,87,97),
+                              ( '1002',95,97,99),
+insert into markdetails values  ( '1004',95,57,47),
+                            ( '1005',95,57,47);
 select * from markdetails
+
+
+
+  /* */
+select phy from markdetails where physics between 50 and 90
+select * from markdetails where math >=50 and math <=90
+
+select * from students where address = 'nagpur'
+select * from students where rollno='1005'
+
+delete from students where rollno='1002'
+delete from markdetails where rollno='1002'
+
+update students set address='nagpur', where rollno='1005'
+
+
+
+
+
 
 
