62d5b8a39e0b63007d1ae89f2a3a0d824e70d90f
pradnyasontakke <pradnyasontakke14@gmail.com>
2023-10-09 17:47:32 +0530

    join

---
+++ SQL/joinAs18.sql
@@ -0,0 +1,8 @@
+18.	Get the film titles and rental dates for all rentals made on August 10, 2005.
+
+
+use sakila
+select title ,rental_date from film join inventory on 
+film.film_id=inventory.inventory_id join rental on 
+inventory.inventory_id=rental.inventory_id join customer on 
+rental.customer_id=customer.customer_id
