59093492d8954642b7601658b8eba9bbf851e40c
pradnyasontakke <pradnyasontakke14@gmail.com>
2023-10-10 13:19:30 +0530

    join

--- SQL/join1As48.sql
+++ SQL/join1As48.sql
@@ -3,8 +3,8 @@
 
 use sakila
 select rental_date,rental_duration, first_name,last_name from film join inventory on 
-film.film_id=inventory.inventory_id join rental on 
+film.film_id=inventory.film_id join rental on 
 inventory.inventory_id=rental.inventory_id join customer on 
 rental.customer_id=customer.customer_id where 
-customer.first_name='mary' and customer.last_name='smith' 
-select first_name,last_name from customer
+customer.first_name='joel' and customer.last_name='francisco' 
+
