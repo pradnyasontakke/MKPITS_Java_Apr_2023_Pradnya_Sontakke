86ea8fd8658239a6665c25bbeaf74f05b64bba88
pradnyasontakke <pradnyasontakke14@gmail.com>
2023-10-20 22:27:28 +0530

    view

---
+++ SQL/view/viewAssignment4.sql
@@ -0,0 +1,6 @@
+/*Assignment 4: Conditional View
+Create a view named new_release_films that displays the film_id, title, and release_year for films released after
+ the year 2005.*/
+ use sakila
+
+
