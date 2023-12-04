f3118fe4847eddac5e6b8b0e1795b4f2eccd3e21
pradnyasontakke <pradnyasontakke14@gmail.com>
2023-12-04 12:02:09 +0530

    jpa_repository

---
+++ JPA/JPAServiceRestRepository/JPAServiceRestRepository/src/main/java/com/example/JPAServiceRestRepository/JpaServiceRestRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.JPAServiceRestRepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaServiceRestRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaServiceRestRepositoryApplication.class, args);
+	}
+
+}
---
+++ JPA/JPAServiceRestRepository/JPAServiceRestRepository/src/test/java/com/example/JPAServiceRestRepository/JpaServiceRestRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JPAServiceRestRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaServiceRestRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
