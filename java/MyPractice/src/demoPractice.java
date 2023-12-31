6450be4de4724d907315f3a26a3512cc3efbb4d3
pradnyasontakke <pradnyasontakke14@gmail.com>
2023-12-31 22:59:13 +0530

    practicebank

---
+++ java/MyPractice/src/demoPractice.java
@@ -0,0 +1,13 @@
+public class demoPractice {
+    public double doubledemo(double n1,double n2) {
+        return n1 + n2;
+    }
+
+    public static void main(String[] args) {
+
+        demoPractice demoPractice=new demoPractice();
+        double add =demoPractice.doubledemo(1.1d,2.2d);
+    	System.out.println(add);
+}  }
+
+
